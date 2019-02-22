57f0adc9f95d0d49887eae07399bab22cfd65c52
calebfergie <yonohabloespanol@gmail.com>
2019-02-22 14:16:12 -0500

    messing with jquery to detect text entry

--- public/js/cfscript.js
+++ public/js/cfscript.js
@@ -1,1 +1,40 @@
-window.alert("This is a test of additional javascript compatibility");
+function contact_function() {
+  var fieldDiv = document.getElementById('passages');
+  var passageDiv = $('input');
+  console.log("fieldDiv is: " + passageDiv);
+}
+
+$(document).on("click", "a", function() {
+  console.log("It works!");
+});
+
+
+
+// $(document.body).delegate('input:text', 'keypress', function(e) {
+//   console.log("pressed enter");
+//     if (e.which === 13) { // if is enter
+//         console.log("pressed enter");
+//         e.preventDefault(); // don't submit form
+//         // do what you want here
+//         contact_function();
+//     }
+// });
+
+// $(document.body).keypress(function( event ) {
+//   if ( event.which == 13 ) {
+//      event.preventDefault();
+//   }
+//   var msg = "Handler for .keypress()";
+//   $.print( msg, "html" );
+//   $.print( event );
+// });
+
+$(document).ready(function() {
+  $(document.body).keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      //do stuff on enter
+      contact_function();
+    }
+  });
+});
