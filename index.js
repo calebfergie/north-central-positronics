a73bbe7bf16e0b1455a936cc6585b12702e47a1c
calebfergie <yonohabloespanol@gmail.com>
2019-02-23 01:53:31 -0500

    updated documentation

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000
 require('dotenv').config()
 
-
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => {res.render('pages/index');console.log(process.env.FAKESECRET);})
+  .get('/', (req, res) => {res.render('pages/index');})
   .post('/db', (req, res) => {console.log("got a post request of " + req.body.answer),res.status(200).end();})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
