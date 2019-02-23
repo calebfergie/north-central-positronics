eb39a5196865a1790c1ea543f5bec2511c0ed763
calebfergie <yonohabloespanol@gmail.com>
2019-02-22 20:02:42 -0500

    added jquery tracking of text responses

--- public/js/cfscript.js
+++ public/js/cfscript.js
@@ -1,40 +1,50 @@
-function contact_function() {
-  var fieldDiv = document.getElementById('passages');
-  var passageDiv = $('input');
-  console.log("fieldDiv is: " + passageDiv);
-}
+function record(event) {
+  var answerDiv = document.getElementById("transfercell");
+  var answer = answerDiv.textContent;
+  console.log("cfscript has detected a new answer: " + answer);
+};
 
-$(document).on("click", "a", function() {
-  console.log("It works!");
-});
-
-
-
-// $(document.body).delegate('input:text', 'keypress', function(e) {
-//   console.log("pressed enter");
-//     if (e.which === 13) { // if is enter
-//         console.log("pressed enter");
-//         e.preventDefault(); // don't submit form
-//         // do what you want here
-//         contact_function();
-//     }
-// });
-
-// $(document.body).keypress(function( event ) {
-//   if ( event.which == 13 ) {
-//      event.preventDefault();
-//   }
-//   var msg = "Handler for .keypress()";
-//   $.print( msg, "html" );
-//   $.print( event );
-// });
-
+// jQuery event for when enter is presed
 $(document).ready(function() {
   $(document.body).keypress(function(event) {
     if (event.which == 13) {
       event.preventDefault();
-      //do stuff on enter
-      contact_function();
+      console.log("user pressed enter");
+      event.preventDefault(); // don't submit form
+      // do what you want here
+      record(event);
     }
   });
 });
+
+
+// $(document).ready(function() {
+// $(document.body).delegate('input:text', 'keypress', function(e) {
+//   console.log("user pressed a key");
+//   if (e.which === 13) { // if is enter
+//     console.log("user pressed enter");
+//     e.preventDefault(); // don't submit form
+//     // do what you want here
+//     record(e);
+//   }
+// });
+// });
+
+// $(document).on(':passageinit', function (ev) {
+// console.log("passage here boo 2");
+// });
+
+// $(document).on("click", "a", function() {
+//   console.log("It works!");
+// });
+
+// $(document.body).keypress(function(event) {
+//   if (event.which == 13) {
+//     event.preventDefault();
+//     //do stuff on enter
+//     contact_function();
+//   }
+// });
+
+//   postrender.hello = function() {
+//   alert("hello!");
