54c828e15befd711bf1ae32eac9757af39f2dd63
calebfergie <yonohabloespanol@gmail.com>
2019-02-23 00:46:41 -0500

    set up post to app server upon data entry

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000
+require('dotenv').config()
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {res.render('pages/index');console.log(process.env.FAKESECRET);})
+  .post('/db', (req, res) => {console.log("got a post request of " + req.body.answer),res.status(200).end();})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/js/cfscript.js
+++ public/js/cfscript.js
@@ -1,7 +1,59 @@
+var path = '/db';
+
+// function to post user information to express app server
+function postToServer(path,params){
+  $.ajax({
+    type:"POST",
+    dataType:"json",
+    contentType: "application/json",
+    data:JSON.stringify(params),
+    url:path,
+})
+.done(function(response){
+      console.log("Response of update: ",response)
+})
+.fail(function(xhr, textStatus, errorThrown){
+      console.log("ERROR: ",xhr.responseText)
+      return xhr.responseText;
+});
+};
+
+// sourced from https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit, in combination with the final submit function from: https://stackoverflow.com/questions/5733808/submit-form-and-stay-on-same-page
+// function postToServer(path, params, method) {
+//    method = method || "post"; // Set method to post by default if not specified.
+//
+//    // The rest of this code assumes you are not using a library.
+//    // It can be made less wordy if you use one.
+//    var form = document.createElement("form");
+//    form.setAttribute("method", method);
+//    form.setAttribute("action", path);
+//
+//    for(var key in params) {
+//        if(params.hasOwnProperty(key)) {
+//            var hiddenField = document.createElement("input");
+//            hiddenField.setAttribute("type", "hidden");
+//            hiddenField.setAttribute("name", key);
+//            hiddenField.setAttribute("value", params[key]);
+//
+//            form.appendChild(hiddenField);
+//        }
+//    }
+//
+//    document.body.appendChild(form);
+//    form.submit(function(){
+//       $.post($(this).attr('action'), $(this).serialize(), function(response){
+//             // do something here on success
+//       },'json');
+//       return false;
+//    });
+// };
+
 function record(event) {
   var answerDiv = document.getElementById("transfercell");
   var answer = answerDiv.textContent;
+  var data = {answer: answer};
   console.log("cfscript has detected a new answer: " + answer);
+  postToServer(path,data);
 };
 
 // jQuery event for when enter is presed
